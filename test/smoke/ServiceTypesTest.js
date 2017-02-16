14369f9bab6cb9a0de9af5c6345aaee98b511428
royrod27 <Roy.Rodriguez@fundacion-jala.org>
2017-02-16 19:35:12 +0000

    One Smoke Test of Resources

--- test/smoke/ServiceTypesTest.js
+++ test/smoke/ServiceTypesTest.js
@@ -1,15 +1,13 @@
 var expect = require('chai').expect;
 var serviceTypes = require('../../lib/features/ServiceTypes');
 
-describe('Smoke Test for Service-Types', function () {
+
+context('Smoke test for Service-Types', function () {
     var expectedStatus = 200;
     this.timeout(5000);
-});
-
-context('GET test', function () {
    it('Get /service-types returns 200', function (done) {
        serviceTypes.getServiceType(function (err, res) {
-           console.log(res.status);
+           expect(res.status).to.equal(expectedStatus);
            done();
        });
    });
