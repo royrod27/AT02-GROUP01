dda8932255a820f95a1e8f47e9ad1f32697cb21a
sergiolanda <sergio.landa@fundacion-jala.org>
2017-02-21 01:59:38 +0000

    finish room-resources

--- test/smoke/RoomResourcesTest.js
+++ test/smoke/RoomResourcesTest.js
@@ -4,16 +4,15 @@ var room_resources = require('../../lib/features/RoomResources');
 var room = require('../../resources/room.json');
 var tokenGenerator = require('../../lib/helpers/TokenGenerator');
 
+before(function (done) {
+    tokenGenerator
+        .generateToken(function (err, res) {
+            done();
+        })
+});
+
 context('Smoke test for resources of rooms', function () {
-    this.timeout(5000);
-
-
-    before(function (done) {
-        tokenGenerator
-            .generateToken(function (err, res) {
-                done();
-            })
-    });
+    this.timeout(10000);
 
     var body = {
         name: "testResource",
@@ -28,23 +27,49 @@ context('Smoke test for resources of rooms', function () {
     beforeEach(function (done) {
         resources.postResources(body, function (err, res) {
             resourceErr = err;
-            resourceRes = res;
+            resourceRes = res.body;
             done();
         });
     });
     afterEach(function (done) {
-        resources.deleteResource(resourceRes.body._id, function (err, res) {
+        resources.deleteResource(resourceRes._id, function (err, res) {
             done();
         });
     });
 
-    it('get resources by id GET /rooms/{roomId}/resources/{:resourceId}', function (done) {
+    it('GET /rooms/{:roomId}/resources/{:roomResourceId}', function (done) {
         var jsonJoin = {
-            resourceId: resourceRes.body._id,
+            resourceId: resourceRes._id,
             quantity: 5
         };
         room_resources.joinRoomResource(room.id, jsonJoin, function (err, res) {
             room_resources.getResourceOfRoom(room.id, res.body.resources[0]._id, function (err, res) {
+                expect(200).to.equal(res.status);
+                done();
+            })
+        })
+    })
+
+    it('PUT /rooms/{:roomId}/resources/{:roomResourceId}', function (done) {
+        var jsonJoin = {
+            resourceId: resourceRes._id,
+            quantity: 5
+        };
+        room_resources.joinRoomResource(room.id, jsonJoin, function (err, res) {
+            room_resources.putResourceOfRoom(room.id, res.body.resources[0]._id,{"quantity": 10}, function (err, res) {
+                expect(200).to.equal(res.status);
+                done();
+            })
+        })
+    })
+
+    it('DELETE /rooms/{:roomId}/resources/{:roomResourceId}', function (done) {
+        var jsonJoin = {
+            resourceId: resourceRes._id,
+            quantity: 5
+        };
+        room_resources.joinRoomResource(room.id, jsonJoin, function (err, res) {
+            room_resources.delResourceOfRoom(room.id, res.body.resources[0]._id, function (err, res) {
                 expect(200).to.equal(res.status);
                 done();
             })
